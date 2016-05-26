65e02d9dee94e92a27e951671983f031784f23f6
Sergei Lebedev <superbobry@gmail.com>
2016-05-26 15:30:41 +0300

    Added 'StridedVector.searchSorted'
      for doing O(log size) lookups on a sorted vector

---
+++ src/main/kotlin/org/jetbrains/bio/viktor/Searching.kt
@@ -0,0 +1,21 @@
+package org.jetbrains.bio.viktor
+
+/**
+ * Returns the insertion index of [target] into a sorted vector.
+ *
+ * If [target] already appears in this vector, the returned
+ * index is just before the leftmost occurrence of [target].
+ */
+fun StridedVector.searchSorted(target: Double): Int {
+    var lo = 0
+    var hi = size
+    while (lo < hi) {
+        val mid = (lo + hi) ushr 1
+        when {
+            target <= this[mid] -> hi = mid
+            else                -> lo = mid + 1
+        }
+    }
+
+    return lo
+}
