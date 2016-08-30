ccfb7d4398bfe606ee170746b6719b933f6a591a
Sergei Lebedev <superbobry@gmail.com>
2016-08-30 18:38:10 +0300

    Removed hand-rolled SSE2/AVX support detection

--- src/loader/cpp/org_jetbrains_bio_viktor_LoaderKt.cpp
+++ src/loader/cpp/org_jetbrains_bio_viktor_LoaderKt.cpp
@@ -1,36 +1,14 @@
-#include <cassert>
-#include <cstdint>
-#include <cpuid.h>
+#include <boost/simd/arch/tags.hpp>
 
 #include "org_jetbrains_bio_viktor_LoaderKt.hpp"
 
 #define JNI_METHOD(rtype, name)                                         \
     JNIEXPORT rtype JNICALL Java_org_jetbrains_bio_viktor_LoaderKt_##name
 
-static inline int64_t xgetbv(int ctr) {
-   uint32_t eax, edx;
-   __asm("xgetbv" : "=a"(eax),"=d"(edx) : "c"(ctr) : );
-   return eax | (uint64_t(edx) << 32);
-}
-
 JNI_METHOD(jboolean, isAvxSupported)(JNIEnv *env, jclass) {
-    uint32_t eax, ebx, ecx, edx;
-    __cpuid_count(0, 0, eax, ebx, ecx, edx);
-    assert(eax >= 0x00000001);
-
-    __cpuid_count(1, 0, eax, ebx, ecx, edx);
-    if ((edx & bit_SSE2) == 0) {
-        return false;
-    }
-
-    return (ecx & bit_XSAVE) != 0 && (xgetbv(0) & 6) != 0;
+    return boost::simd::avx.is_supported();
 }
 
 JNI_METHOD(jboolean, isSse2Supported)(JNIEnv *env, jclass) {
-    uint32_t eax, ebx, ecx, edx;
-    __cpuid_count(0, 0, eax, ebx, ecx, edx);
-    assert(eax >= 0x00000001);
-
-    __cpuid_count(1, 0, eax, ebx, ecx, edx);
-    return (ecx & bit_SSE2) != 0;
+    return boost::simd::sse2.is_supported();
 }
