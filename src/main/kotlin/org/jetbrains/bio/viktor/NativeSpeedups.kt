f39a305a96d8699f7d3e7d963a62d05cdd2e2de2
dievsky <dievsky@gmail.com>
2021-04-13 15:06:09 +0200

    Added complex multiplication and FFT to Kotlin

--- src/main/kotlin/org/jetbrains/bio/viktor/NativeSpeedups.kt
+++ src/main/kotlin/org/jetbrains/bio/viktor/NativeSpeedups.kt
@@ -43,4 +43,16 @@ internal object NativeSpeedups {
     external fun unsafeSD(values: DoubleArray, offset: Int, length: Int): Double
 
     external fun unsafeCumSum(dest: DoubleArray, destOffset: Int, length: Int): Boolean
+
+    external fun unsafeFFT(dst: DoubleArray, dstOffset: Int, src: DoubleArray, srcOffset: Int, length: Int): Boolean
+
+    external fun unsafeComplexTimes(
+        dst: DoubleArray,
+        dstOffset: Int,
+        src1: DoubleArray,
+        srcOffset1: Int,
+        src2: DoubleArray,
+        srcOffset2: Int,
+        length: Int
+    ): Boolean
 }
