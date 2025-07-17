30c34157dc192c64f7658d661567664c35bc93f9
Oleg Shpynov <oleg.shpynov@jetbrains.com>
2025-07-17 11:55:22 +0200

    Updated publish urls to ossr-staging-api

--- build.gradle.kts
+++ build.gradle.kts
@@ -130,8 +130,8 @@ configure<PublishingExtension> {
 
     repositories {
         maven {
-            val releasesRepoUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2/"
-            val snapshotsRepoUrl = "https://oss.sonatype.org/content/repositories/snapshots/"
+            val releasesRepoUrl = "https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/"
+            val snapshotsRepoUrl = "https://central.sonatype.com/repository/maven-snapshots/"
             url = uri(if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl)
 
             credentials {
